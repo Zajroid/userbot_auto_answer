aafd4a6585c19c0247e2aeca21f754fbbb38a09e
zajroid <zajroid@gmail.com>
2022-10-17 11:13:54 +0300

    change from lower in uppercase const variables

--- main.py
+++ main.py
@@ -4,65 +4,32 @@ import random
 import json
 import requests 
 
-
 from distutils.command.config import config
 from configparser import ConfigParser
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from asyncio import sleep
-
-
 from bs4 import BeautifulSoup 
-
-
-from auth_data import token
 from vk.exceptions import VkAPIError
-
-
-
-
 
 
 config = ConfigParser()
 config.read('./systemd/config.ini')
 
 
-OPEN_WEATHER_MAP_KEY = 'c7a4efa957dc6ce374e501178e5ee198'
+NAME = config.get('pyrogram', 'name')
+API_ID = config.get('pyrogram', 'api_id')
+API_HASH = config.get('pyrogram', 'api_hash')
+PHONE_NUMBER = config.get('pyrogram', 'phone_number')
+
+TOKEN_VK = config.get('vk', 'token')
+OPEN_WEATHER_MAP_KEY = config.get('openweathermap', 'OPEN_WEATHER_MAP_KEY')
 
 
-def parsing_vk():
-    api = vk.API(access_token=token, v='5.131')
-
-    posts = api.wall.get(domain="english_with_pleasure", count=1)
-
-    print(posts.items())
-
-    for i in posts['items']:
-        return str(i['text'])
-
-
-
-def parsing_test():
-    response = requests.get("https://duckduckgo.com/") 
-    soup = BeautifulSoup(response.content, 'html.parser') 
-        
-    return soup.title.string
-
-
-
-# print(ddd)
-
-
-name = config.get('pyrogram', 'name')
-api_id = config.get('pyrogram', 'api_id')
-api_hash = config.get('pyrogram', 'api_hash')
-phone_number = config.get('pyrogram', 'phone_number')
-
-
-app = Client(name=name, 
-             api_id=api_id, 
-             api_hash=api_hash, 
-             phone_number=phone_number)
+app = Client(name=NAME, 
+             api_id=API_ID, 
+             api_hash=API_HASH, 
+             phone_number=PHONE_NUMBER)
 
 
 # ask
@@ -84,6 +51,26 @@ idk_answer_list = ['я не знаю ответа', 'я просто цифры'
 punctuation_list = ['?',',','.','...',':',';','#','$']
 punctuation_answer_list = ['???']
 
+
+def parsing_vk():
+    api = vk.API(access_token=str(TOKEN_VK).strip('\"'), v='5.131')
+
+    posts = api.wall.get(domain="english_with_pleasure", count=1)
+
+    print(posts.items())
+
+    for i in posts['items']:
+        return str(i['text'])
+
+
+def parsing_test():
+    response = requests.get("https://duckduckgo.com/") 
+    soup = BeautifulSoup(response.content, 'html.parser') 
+        
+    return soup.title.string
+
+
+
 @app.on_message(filters=filters.dice & filters.incoming & filters.private)
 async def dice(client, message):
     player_1 = message # usr
@@ -101,63 +88,43 @@ async def dice(client, message):
 
 @app.on_message(filters=filters.private & filters.incoming & filters.regex('[а-яА-Яa-zA-z]'))
 async def auto_answer(client: Client, message: Message):
-    mci = message.chat.id
-    
     if message.text.lower() in greeting_dict:
-        await app.send_message(mci, random.choice(greeting_answer_list).capitalize())
+        await app.send_message(message.chat.id, random.choice(greeting_answer_list).capitalize())
     elif message.text.lower() in fatewell_dict:
-        await app.send_message(mci, random.choice(fatewell_answer_list).capitalize())
+        await app.send_message(message.chat.id, random.choice(fatewell_answer_list).capitalize())
     elif message.text.lower() in how_are_doing_list:
-        await app.send_message(mci, random.choice(how_are_doing_answer_list).capitalize())
+        await app.send_message(message.chat.id, random.choice(how_are_doing_answer_list).capitalize())
     elif message.text.lower() in punctuation_list:
-        await app.send_message(mci, random.choice(punctuation_answer_list).capitalize())
+        await app.send_message(message.chat.id, random.choice(punctuation_answer_list).capitalize())
     elif message.text.lower() == "$cat":
-        await client.send_sticker(chat_id=mci, sticker='tgbot/stickers/sticker.webp')
+        await client.send_sticker(chat_id=message.chat.id, sticker='tgbot/stickers/sticker.webp')
     elif message.text.lower() == "что делаешь?":
-        await client.send_sticker(chat_id=mci, sticker='tgbot/stickers/cats/i_busy.webp')
+        await client.send_sticker(chat_id=message.chat.id, sticker='tgbot/stickers/cats/i_busy.webp')
         time.sleep(0.5)
-        await client.send_message(mci, 'busy')
-    # elif message.text.lower() == "$pars":
-    #     ddd = parsing_test()
-    #     await app.send_message(mci, ddd)
-    # elif message.text.lower() == "$pars_vk":
-    #     await app.send_message(mci, parsing_vk())
+        await client.send_message(message.chat.id, 'busy')
     else:
-        await app.send_message(mci, random.choice(idk_answer_list).capitalize())
+        await app.send_message(message.chat.id, random.choice(idk_answer_list).capitalize())
 
 
-@app.on_message(filters=filters.private & filters.outgoing & filters.regex('[а-яА-Яa-zA-z]'))
+@app.on_message(filters=filters.private & filters.outgoing & filters.regex('[а-яА-Яa-zA-z]') & filters.command('$pars_vk'))
 async def auto_answer(client: Client, message: Message):
-    mci = message.chat.id
-    
-    if message.text.lower() == "$pars_vk":
-        ddd = parsing_vk()
-        await app.send_message(mci, ddd)
-    else:
-        await app.send_message(mci, 'не вышло(')
-
-
-
-@app.on_message(filters=filters.private & filters.outgoing & filters.regex('[а-яА-Яa-zA-z]'))
-async def auto_answer(client: Client, message: Message):
-    mci = message.chat.id
-    
     if message.text.lower() == "$pars":
-        ddd = parsing_test()
-        await app.send_message(mci, ddd)
+        prs_test = parsing_test()
+        await app.send_message(message.chat.id, prs_test)
     else:
-        await app.send_message(mci, 'не вышло(')
-        
+        await app.send_message(message.chat.id, 'не вышло(')
 
 
+@app.on_message(filters=filters.private & filters.outgoing & filters.regex('[а-яА-Яa-zA-z]') & filters.command('$pars') )
+async def auto_answer(client: Client, message: Message):
+    prs_vk = parsing_vk()
+    await app.send_message(message.chat.id, prs_vk)
 
 
 @app.on_message(filters=filters.private & filters.incoming)
-async def auto_answer(client: Client, message: Message):
-    mci = message.chat.id
-    
+async def auto_answer(client: Client, message: Message):    
     if message.text.lower() == "123":
-        await app.send_message(mci, "321")
+        await app.send_message(message.chat.id, "321")
 
 
 if __name__=='__main__':
