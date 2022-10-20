8bddf264c45e05b62fc24b1f1e60626f6b988f3e
zajroid <zajroid@gmail.com>
2022-10-20 21:13:19 +0300

    add .help

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import vk
 import time
 import random
-import json
-import requests 
-import lxml
+import requests
+import subprocess
 
-from distutils.command.config import config
 from configparser import ConfigParser
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from asyncio import sleep
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 from vk.exceptions import VkAPIError
 from loguru import logger
 
@@ -18,10 +16,10 @@ from loguru import logger
 config = ConfigParser()
 config.read('./systemd/config.ini')
 
-logger.add('./logs/logers.log', 
+logger.add('./logs/loggers.log',
            format="{time} {level} {message}",
-           level="INFO", 
-           rotation="1 MB", 
+           level="INFO",
+           rotation="1 MB",
            compression="zip")
 
 
@@ -33,12 +31,10 @@ PHONE_NUMBER = config.get('pyrogram', 'phone_number')
 TOKEN_VK = config.get('vk', 'token')
 OPEN_WEATHER_MAP_KEY = config.get('openweathermap', 'OPEN_WEATHER_MAP_KEY')
 
-# print(NAME, API_ID, API_HASH, PHONE_NUMBER, TOKEN_VK, OPEN_WEATHER_MAP_KEY, sep="\n")
 
-
-app = Client(name=NAME, 
-             api_id=API_ID, 
-             api_hash=API_HASH, 
+app = Client(name=NAME,
+             api_id=API_ID,
+             api_hash=API_HASH,
              phone_number=PHONE_NUMBER)
 
 
@@ -58,21 +54,21 @@ how_are_doing_answer_list = ['нормально, а ты?', 'отлично, а
 idk_answer_list = ['я не знаю ответа', 'я просто цифры', 'не шарю', 'и что это?', '-_-']
 
 # punctuation
-punctuation_list = ['?',',','.','...',':',';','#','$']
+punctuation_list = ['?', ',', '.', '...', ':', ';', '#', '$']
 punctuation_answer_list = ['???']
 
 
-class VK(object):
-    def parsing_vk(self):
-        api = vk.API(access_token=str(TOKEN_VK).strip('\"'), v='5.131')
-        posts = api.wall.get(domain="english_with_pleasure", count=1)
-        for i in posts['items']:
-            return str(i['text'])
+
+def parsing_vk():
+    api = vk.API(access_token=str(TOKEN_VK), v='5.131')
+    posts = api.wall.get(domain="english_with_pleasure", count=1)
+    for i in posts['items']:
+        return str(i['text'])
 
 
 def parsing_test():
-    response = requests.get("https://duckduckgo.com/") 
-    soup = BeautifulSoup(response.content, 'html.parser') 
+    response = requests.get("https://duckduckgo.com/")
+    soup = BeautifulSoup(response.content, 'html.parser')
     return soup.title.string
 
 
@@ -93,14 +89,14 @@ def parsing_english_native():
             link_articles = "https://www.native-english.ru" + link_a.get("href")
             test.append(f"{link_articles_title}: {link_articles}")
             counter += 1
-        
+
     return str(test)
 
 
 @app.on_message(filters=filters.dice & filters.incoming & filters.private)
 async def dice(client, message):
-    player_1 = message # usr
-    player_2 = (await app.send_dice(message.chat.id)) # bot
+    player_1 = message  # usr
+    player_2 = (await app.send_dice(message.chat.id))  # bot
 
     await sleep(3.5)
 
@@ -110,7 +106,7 @@ async def dice(client, message):
         await app.send_message(message.chat.id, 'Ты проиграл')
     else:
         await app.send_message(message.chat.id, 'Ничья')
-        
+
 
 @app.on_message(filters=filters.private & filters.incoming & filters.text)
 async def auto_answer(client: Client, message: Message):
@@ -135,13 +131,14 @@ async def auto_answer(client: Client, message: Message):
 @app.on_message(filters.command('pars_vk', prefixes='$'))
 async def auto_answer(client: Client, message: Message):
     try:
-        vk = VK()
+        prs_vk = parsing_vk()
         logger.info(f"Message has been send {message.chat.id}")
-        await app.send_message(message.chat.id, vk.parsing_vk())
+        await app.send_message(message.chat.id, prs_vk)
     except Exception as e:
         logger.error(f"{e}")
         await app.send_message(message.chat.id, "Не сейчас")
-        
+
+
 @app.on_message(filters=filters.voice)
 async def ddd(client: Client, message: Message):
     try:
@@ -154,30 +151,39 @@ async def ddd(client: Client, message: Message):
 
 @app.on_message(filters.command('pars', prefixes='$'))
 async def auto_answer(client: Client, message: Message):
-    ddd = parsing_test()
-    await app.send_message(message.chat.id, ddd)
-    
-    
-@app.on_message(filters.command('preng', prefixes='$'))
+    prs_test = parsing_test()
+    await app.send_message(message.chat.id, prs_test)
+
+
+# send list of grammaticals links in English
+@app.on_message(filters.command('preng', prefixes='.'))
 async def auto_answer(client: Client, message: Message):
-    ddd = parsing_english_native()
-    await app.send_message(message.chat.id, ddd, disable_web_page_preview=True)
+    prs_eng_ntv = parsing_english_native().strip("[]\'")
+    await app.send_message(message.chat.id, prs_eng_ntv, disable_web_page_preview=True)
 
 
 @app.on_message(filters=filters.command('vc1', prefixes='.'))
 async def vc1(client: Client, message: Message):
     await app.send_voice(message.chat.id, "audio_2022-10-17_11-54-05.ogg")
-    
 
-@app.on_message(filters=filters.command('vc2', prefixes='.'))
+
+
+help_msg = """
+    Hi, Im Zajroid and I simple bot
+How can I help you?
+Enter next commands:
+  1) .preng - the bot will sends a list of grammaticals links in English for you 🎓
+  2)
+"""
+
+
+@app.on_message(filters=filters.command('help', prefixes='.'))
 async def vc1(client: Client, message: Message):
-    await app.send_voice(message.chat.id, "audio_2022-10-17_11-54-05.ogg")
-    
-    
-@app.on_message(filters=filters.command('vc3', prefixes='.'))
-async def vc1(client: Client, message: Message):
-    await app.send_voice(message.chat.id, "audio_2022-10-17_11-54-05.ogg")
+    await app.send_message(message.chat.id, help_msg)
 
 
-if __name__=='__main__':
-    app.run()
+if __name__ == '__main__':
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        exit()
